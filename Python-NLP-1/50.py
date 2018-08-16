b4cd21c42c4e13933d6fedf5021a7dad46f3c5bc
Saumay Paul <saumaypaul20@gmail.com>
2018-08-17 01:20:18 +0530

    Add files via upload

---
+++ Python-NLP-1/50.py
@@ -0,0 +1,29 @@
+'''
+Done By:-
+            SAUMAY PAUL         CSE 50-14
+            SHAHRUKH RAHMAN     CSE 52-14
+'''
+
+from nltk.stem import PorterStemmer,LancasterStemmer,RegexpStemmer, WordNetLemmatizer
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+ 
+ps = PorterStemmer()
+ls = LancasterStemmer()
+
+
+lemmatiser = WordNetLemmatizer()
+
+f=open("text.txt", "r")
+if f.mode == 'r':
+    contents =f.read()
+
+tokens = word_tokenize(contents) # Generate list of tokens
+
+for i in tokens:
+    print('GIVEN WORD:           %s' %i.upper())
+    print('Porter Stemmer:       %s' %ps.stem(i))
+    print('Lancaster Stemmer:    %s' %ls.stem(i))
+    print('WordNetLemmatizer:    %s '% lemmatiser.lemmatize(i))
+    print('')
+    
